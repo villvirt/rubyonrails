375947de2b47f7ea855e24e89deb94d6f2302618
wadror <wadror@wadror-VirtualBox.(none)>
2014-01-19 20:37:24 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ratebeer::Application.config.secret_key_base = 'ca8a259898ee1107f1478cc4b69b43c13cb9db57fb3c0a81534225de99e2db98ff651c21c9381b23ffbdc68cd804ac7d4e1b7b548a167bef92783dcb4a3c2586'
