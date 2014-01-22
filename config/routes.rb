25207f4dc3ee6156648616df528ac9eace1dc134
wadror <wadror@wadror-VirtualBox.(none)>
2014-01-22 22:51:38 +0200

    asd

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Ratebeer::Application.routes.draw do
   resources :beers
 
   resources :breweries
-
+  
+  root 'breweries#index'
+  get 'ratings' , to: 'ratings#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
